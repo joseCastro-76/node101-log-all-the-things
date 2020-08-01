97cab40d1727789b0dd8498fb9536229a9114656
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-07-31 17:05:47 -0700

    initial upload.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
+const log = [];
+fs
+    .createReadStream(__dirname + '/log.csv')
+    .pipe(csv())
+    .on('data', (row) => log.push(row))
 
+app.use((req, res, next) => {
+    const agent = req.get('user-agent').replace(/\,/g,"");
+    const date = new Date();
+    const time = date.toISOString();
+    const method = req.method;
+    const resource = req.path;
+    const version = `HTTP/${req.httpVersion}`;
+    const status = 200;
+    const payload = `\n${agent},${time},${method},${resource},${version},${status}`;
+
+    fs
+        .appendFile(__dirname + '/log.csv', payload, (err) => {
+            if(err) throw err;
+        })  
+
+    fs
+        .createReadStream(__dirname + '/log.csv')
+        .pipe(csv())
+        .on('data', (row) => log.push(row))
+
+    next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+    console.log(log);
+    res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+    res.json(log);
 
 });
 
